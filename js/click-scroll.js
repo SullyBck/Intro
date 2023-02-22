11bffb72d0df949b364e860ce7635a89334b0ea2
Sully Bck <sully.bck@gmail.com>
2023-02-23 00:22:39 +0100

    carousel enfin bien

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,37 +1,39 @@
-//jquery-click-scroll
-//by syamsul'isul' Arifin
-
-var sectionArray = [1, 2, 3, 4, 5];
-
-$.each(sectionArray, function(index, value){
-          
-     $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 90;
-         var docScroll = $(document).scrollTop();
-         var docScroll1 = docScroll + 1;
-         
-        
-         if ( docScroll1 >= offsetSection ){
-             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-         }
-         
-     });
-    
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 90;
-        e.preventDefault();
-        $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
+// Tableau d'objets pour les sections
+var sections = [
+    {id: 'accueil', name: 'Accueil'},
+    {id: 'presentation', name: 'Présentation'},
+    {id: 'informatique', name: 'Informatique'},
+    {id: 'video', name: 'Vidéo'},
+    {id: 'programmation', name: 'Programmation'}
+  ];
+  
+  // Parcourir les sections et ajouter des écouteurs d'événements de défilement et de clic
+  $.each(sections, function(index, section) {
+    $(document).scroll(function(){
+      var offsetSection = $('#' + section.id).offset().top - 90;
+      var docScroll = $(document).scrollTop();
+      var docScroll1 = docScroll + 1;
+  
+      if (docScroll1 >= offsetSection) {
+        $('.navbar-nav .nav-item .nav-link').removeClass('active');
+        $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
+        $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+        $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+      }
     });
-    
-});
-
-$(document).ready(function(){
+  
+    $('.click-scroll[href="#' + section.id + '"]').click(function(e){
+      var offsetClick = $('#' + section.id).offset().top - 90;
+      e.preventDefault();
+      $('html, body').animate({
+        'scrollTop':offsetClick
+      }, 300)
+    });
+  });
+  
+  // Définir le lien de la barre de navigation active lors du chargement de la page
+  $(document).ready(function(){
     $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
     $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
     $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
-});
+  });
