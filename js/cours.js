624a9b7be99d1603bd2d412cb9c79059e6b85a71
Sully Bck <sullyvan.lb@gmail.com>
2023-08-28 18:31:26 +0200

    cours et info beta

--- js/cours.js
+++ js/cours.js
@@ -56,37 +56,3 @@ tabs2.forEach(tab2 => {
     });
 });
 
-function setIframeHeight(iframeId) {
-    var iframe = document.getElementById(iframeId);
-    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-    if (windowWidth > 991.98) {
-        if (windowHeight > 1120) {
-            iframe.style.height = (1120 * 0.8) + "px"; // Utilise 80% de la hauteur si la largeur est supérieure à 1120px
-        } else {
-            iframe.style.height = (windowHeight * 0.8) + "px"; // Utilise 80% de la hauteur dans les autres cas
-        }
-    } else if (windowWidth < 400) {
-        if (windowHeight < 720) {
-            iframe.style.height = (windowHeight * 0.75) + "px"; // Utilise 75% de la hauteur dans les autres cas
-        } else {
-            iframe.style.height = (720 * 0.75) + "px"; // Utilise 75% de la hauteur si la largeur est inférieure à 400px et la hauteur est supérieure à 720px
-        }
-    } else {
-        iframe.style.height = (windowHeight * 0.75) + "px"; // Utilise 75% de la hauteur si la largeur est entre 400px et 991.98px
-    }
-}
-
-// Appelle la fonction lors du chargement initial de la page et lors du redimensionnement de la fenêtre
-window.addEventListener("load", function () {
-    setIframeHeight("pdf1");
-    setIframeHeight("pdf2"); // Assurez-vous d'ajouter tous les ID d'iframe nécessaires ici
-});
-
-window.addEventListener("resize", function () {
-    setIframeHeight("pdf1");
-    setIframeHeight("pdf2"); // Assurez-vous d'ajouter tous les ID d'iframe nécessaires ici
-});
-
-
