d7fc9b6dadc708690657c32bf819ae8c5ce4cfa8
Sully Bck <sullyvan.lb@gmail.com>
2023-08-28 12:18:39 +0200

    cours mobile

--- js/cours.js
+++ js/cours.js
@@ -1,6 +1,6 @@
 function changePDF(newPDFPath) {
-    var pdfViewer = document.getElementById('pdfViewer');
-    pdfViewer.setAttribute('src', newPDFPath);
+    var pdf1 = document.getElementById('pdf1');
+    pdf1.setAttribute('src', newPDFPath);
 }
 
 const tabs = document.querySelectorAll('.tab');
@@ -28,8 +28,8 @@ tabs.forEach(tab => {
 });
 
 function changePDF2(newPDFPath2) {
-    var pdfViewer2 = document.getElementById('pdfViewer2');
-    pdfViewer2.setAttribute('src', newPDFPath2);
+    var pdf2 = document.getElementById('pdf2');
+    pdf2.setAttribute('src', newPDFPath2);
 }
 
 const tabs2 = document.querySelectorAll('.tab2');
@@ -55,3 +55,38 @@ tabs2.forEach(tab2 => {
         });
     });
 });
+
+function setIframeHeight(iframeId) {
+    var iframe = document.getElementById(iframeId);
+    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    if (windowWidth > 991.98) {
+        if (windowHeight > 1120) {
+            iframe.style.height = (1120 * 0.8) + "px"; // Utilise 80% de la hauteur si la largeur est supérieure à 1120px
+        } else {
+            iframe.style.height = (windowHeight * 0.8) + "px"; // Utilise 80% de la hauteur dans les autres cas
+        }
+    } else if (windowWidth < 400) {
+        if (windowHeight < 720) {
+            iframe.style.height = (windowHeight * 0.75) + "px"; // Utilise 75% de la hauteur dans les autres cas
+        } else {
+            iframe.style.height = (720 * 0.75) + "px"; // Utilise 75% de la hauteur si la largeur est inférieure à 400px et la hauteur est supérieure à 720px
+        }
+    } else {
+        iframe.style.height = (windowHeight * 0.75) + "px"; // Utilise 75% de la hauteur si la largeur est entre 400px et 991.98px
+    }
+}
+
+// Appelle la fonction lors du chargement initial de la page et lors du redimensionnement de la fenêtre
+window.addEventListener("load", function () {
+    setIframeHeight("pdf1");
+    setIframeHeight("pdf2"); // Assurez-vous d'ajouter tous les ID d'iframe nécessaires ici
+});
+
+window.addEventListener("resize", function () {
+    setIframeHeight("pdf1");
+    setIframeHeight("pdf2"); // Assurez-vous d'ajouter tous les ID d'iframe nécessaires ici
+});
+
+
